31fc71cf5a75c45b06a98c9cf0dc90e8979bbb23
JayeshDhawade <150729752+JayeshDhawade@users.noreply.github.com>
2026-07-20 12:30:15 +0530

    Added Storage account

--- main.tf
+++ main.tf
@@ -25,3 +25,11 @@ resource "azurerm_resource_group" "demo" {
   name     = "rg-portfolio-demo"
   location = "centralindia"
 }
+
+resource "azurerm_storage_account" "demo" {
+  name                     = "stportfoliodemo01"  # must be globally unique - change if needed
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
